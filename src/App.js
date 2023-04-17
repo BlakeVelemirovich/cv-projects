dc9615d83b80180115381b932b11bbba2d40dde3
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-04-16 21:03:22 -0300

    Basic text editing functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [edit, setEdit] = useState(false);
+  const [text, setText] = useState('Hello World');
+
+  const handleEdit = () => {
+    setEdit(!edit);
+  }
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
   return (
     <div className="App">
-      Hello World
+      {!edit ? text : <input type="text" value={text} onChange={handleChange}/>} 
+      <button onClick={handleEdit}>edit</button>
     </div>
   );
 }
